4bce05093a6fb510175611dc0b6753a633304150
lskervin <156468489+lskervin@users.noreply.github.com>
2024-05-15 21:19:03 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,44 @@
+# Standard library imports
+
+# Remote library imports
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from dotenv import load_dotenv
+import os
+# Local imports
+# Load environment variables from .env file
+load_dotenv()
+# Instantiate app, set attributes
+app = Flask(__name__)
+# set a secret key (needed for browser cookies)
+app.secret_key = os.environ['SECRET_KEY']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+# Define metadata, instantiate db
+metadata = MetaData(naming_convention={
+    
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+# Instantiate Bcrypt
+bcrypt = Bcrypt(app)
+
+# Instantiate REST API
+api = Api(app)
+
+# Instantiate CORS
+CORS(app, supports_credentials=True)
+
+#making a few changes so git with be tricked
